ad0f55420edbe1dc635654ece71eeee607a4360c
Cartosn <carlosdanielguarin@gmail.com>
2023-02-16 17:40:46 -0500

    ._.

--- main.py
+++ main.py
@@ -33,12 +33,12 @@ class Motor:
 class Auto:
     cantidadCreados = 0
 
-    def __init__(self, modelo, precio, marca, registro):
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
         self.modelo = modelo
         self.precio = precio
-        self.asientos = [Asiento]
+        self.asientos = [asientos]
         self.marca = marca
-        self.motor = Motor
+        self.motor = motor
         self.registro = registro
 
     def cantidadAsientos(self):
